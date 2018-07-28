ece2bbce6433f15881b4284e3b38a5d42698eae6
poerty <s2peters2@naver.com>
2018-07-28 21:10:32 +0900

    gensim - lda topic generate. (hyper parameter? 결정 x)

--- python/main.py
+++ python/main.py
@@ -5,15 +5,30 @@ from bs4 import BeautifulSoup
 import urllib.request
 import urllib.parse
 import re
+import lda
+from datetime import timedelta, date
+def daterange(date1, date2):
+    for n in range(int((date2-date1).days)+1):
+        yield date1 + timedelta(n)
 
 from konlpy.tag import Hannanum
 hannanum=Hannanum()
 
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models
+import gensim
+p_stemmer=PorterStemmer()
+
 daum_url='http://media.daum.net/breakingnews/'
 daum_url2='?regDate='
-categoryList=['society','digital']
-dateList=['20180704','20180710']
+categoryList=['society']
+dateList=[]
+start_dt = date(2018, 5, 1)
+end_dt = date(2018, 7, 25)
+for dt in daterange(start_dt, end_dt):
+    dateList.append(dt.strftime("%Y%m%d"))
 
+ddlist=[]
 for category in categoryList:
     for date in dateList:
         web_url=daum_url+category+daum_url2+date
@@ -39,4 +54,14 @@ for category in categoryList:
                 p=re.compile('\(.*?\)')
                 word_list=[re.sub(p,"",x) for x in hannanum.nouns(a.text) if len(x)>=2]
                 word_list=[x for x in word_list if not('(' in x) and not(')' in x) and not(']' in x) and not('[' in x) and not('>' in x) and not('<' in x) and not('@' in x)]
-                print(word_list)
+                #print(word_list)
+                ddlist.append(word_list)
+#print(ddlist)
+dictionary=corpora.Dictionary(ddlist)
+corpus=[dictionary.doc2bow(doc) for doc in ddlist]
+#print(dictionary)
+#print(corpus)
+
+ntopics=100
+LDA_MODEL=gensim.models.ldamodel.LdaModel(corpus,num_topics=ntopics,id2word=dictionary)
+print(LDA_MODEL.show_topics())
